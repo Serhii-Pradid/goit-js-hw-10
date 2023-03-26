c66cc0953e0f64752c6bce4dfdb451f52e11a51f
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-03-26 23:28:16 +0300

    added countryList

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,24 +1,23 @@
 
 
-function fetchCountries (countryName)  { 
+export const fetchCountries = countryName =>  {
 
 const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
 
 return fetch(url).then(response => {
-    console.log(response.json())
+    //console.log(response)
 
     if(!response.ok) {
         throw new Error(response.status);
     }
-
     return response.json();
  })
-.then(country => {
+
+/*.then(country => {
     console.log(country)
 })
 .catch(error => {
     console.log(error);
-})
-}
+})*/
 
-export default { fetchCountries }
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+import {fetchCountries} from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -8,12 +10,36 @@ const countryListEl = document.querySelector('.country-list')
 const countryWrapperEl = document.querySelector('.country-info');
 
 
-searchForm.addEventListener('input' , onInputCountryName)
+searchForm.addEventListener('input' , debounce(onInputCountryName, DEBOUNCE_DELAY));
 
   function onInputCountryName(event) {
       
       const countryValue = event.target.value.trim();
-      console.log(countryValue)
+      //console.log(countryValue)
   
   fetchCountries(countryValue)
-        }
+  .then(country => {
+    console.log(country);
+    if(country.length > 10) {
+        Notify.info("Too many matches found. Please enter a more specific name."
+        );
+        return;
+    }
+createCounrtyList(country);
+})
+.catch(error => {
+    Notify.failure("Oops, there is no country with that name")
+    console.log(error);
+})
+}
+
+const createCounrtyList = data => {
+    return data.map(
+        ({ flags, name }) => 
+    `<li><img src="${flags.svg}" alt="${name.official}">${name.official}</li>`
+    )
+    .join("");
+};
+
+
+//const createCountryInfo
